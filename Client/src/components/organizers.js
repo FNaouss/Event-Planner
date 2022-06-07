47e827004335edcfddc4b676c4d1aa08c192c008
FNaouss <Nacereddinefardous@gmail.Com>
2022-06-07 22:46:41 +0100

    second 2.0

--- Client/src/components/organizers.js
+++ Client/src/components/organizers.js
@@ -204,11 +204,7 @@ function Organizers() {
               <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                 <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                   {organizers.map((organizer) => (
-                    <a
-                      key={organizer._id}
-                      href={organizer.email}
-                      className="group"
-                    >
+                    <a key={organizer._id} className="group">
                       <h3 className="mt-4 text-sm text-gray-700">
                         {organizer.name}
                       </h3>
